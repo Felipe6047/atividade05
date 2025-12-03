fb1b62a9abb87fef2f0db93732390c49a86b9340
Barusu <ff063836421@gmail.com>
2025-12-03 17:48:26 -0300

    triste porem feliz

--- atividade-01teste/script.js
+++ atividade-01teste/script.js
@@ -4,3 +4,26 @@ document.querySelectorAll('.delete-btn').forEach(button => {
         this.parentElement.remove();
     });
 });
+
+
+const taskInput = document.getElementById("task-input");
+const clickButton = document.getElementById("add-task");
+const tasklist = document.getElementById("task-list");
+
+const listatasks = [];
+clickButton.addEventListener("click",() => {
+listatasks.push(taskInput.value);
+addtask()
+console.log(listatasks);
+
+});
+
+function addtask() {
+    var li = document.createElement ("li")
+    listatasks.forEach(element => {
+        li.innerHTML = `<li>${element}</li>`
+    });
+    console.log(li);
+    tasklist.appendChild(li);
+    
+}
